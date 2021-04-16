9975a0c01f0d2ec9556f10717c8df670de29965a
ashikka <ashikagupta28@gmail.com>
2021-04-16 15:10:56 +0530

    feat: Endpoint for LogEntries

--- src/main/kotlin/com/quipalup/katydid/logentry/primaryadapter/httprest/CreateLogEntryEndpoint.kt
+++ src/main/kotlin/com/quipalup/katydid/logentry/primaryadapter/httprest/CreateLogEntryEndpoint.kt
@@ -1,27 +1,34 @@
 package com.quipalup.katydid.logentry.primaryadapter.httprest
 
+import com.quipalup.katydid.common.domain.Id
+import com.quipalup.katydid.logentry.domain.LogEntry
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
+
+
 
 @RestController
 class CreateLogEntryEndpoint {
     @PostMapping("/log-entries")
     @ResponseStatus(HttpStatus.CREATED)
-    fun execute(): String = """
-        {
-          "data": 
-              {
-                "id": "5ee62461-adb8-4618-a110-06290a787223",
-                "type": "log-entry",
-                "attributes": {
-                  "time": 123345534,
-                  "description": "Yogurt with strawberries",
-                  "amount": 100,
-                  "unit": "percentage"
-                }
-              }
+    fun execute(@RequestBody newLog: LogEntry): MutableList<LogEntryResource> {
+
+        val log = LogEntryResource(
+            id = Id(),
+            type = JsonApiTypes.LOG_ENTRY,
+            attributes = LogEntry(
+                time = newLog.time,
+                description = newLog.description,
+                amount = newLog.amount,
+                unit = newLog.unit,
+            )
+        )
+            val logEntries: MutableList<LogEntryResource> = ArrayList()
+            logEntries.add(log);
+            return logEntries;
         }
-    """.trimIndent()
-}
+    }
+
