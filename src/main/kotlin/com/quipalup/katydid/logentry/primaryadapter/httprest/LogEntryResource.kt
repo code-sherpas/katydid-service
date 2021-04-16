ed4891b1d8f1572d48490a579e9d74297de881b5
ashikka <ashikagupta28@gmail.com>
2021-04-16 15:13:11 +0530

    feat: Added LogEntryResource

--- src/main/kotlin/com/quipalup/katydid/logentry/primaryadapter/httprest/LogEntryResource.kt
+++ src/main/kotlin/com/quipalup/katydid/logentry/primaryadapter/httprest/LogEntryResource.kt
@@ -1,3 +1,10 @@
 package com.quipalup.katydid.logentry.primaryadapter.httprest
 
-data class LongEntryResource()
+import com.quipalup.katydid.common.domain.Id
+import com.quipalup.katydid.logentry.domain.LogEntry
+
+data class LogEntryResource(
+    val id: Id,
+    val type: String,
+    val attributes: LogEntry
+)
