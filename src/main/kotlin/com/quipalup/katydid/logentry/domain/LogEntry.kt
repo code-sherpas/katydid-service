f328b18cb839bc2cdbf0275c4290f0e5304089b5
Ashikka Gupta <58368421+ashikka@users.noreply.github.com>
2021-06-12 13:48:10 +0530

    refactor: Domain layer for generalized log entries

--- src/main/kotlin/com/quipalup/katydid/logentry/domain/LogEntry.kt
+++ src/main/kotlin/com/quipalup/katydid/logentry/domain/LogEntry.kt
@@ -9,3 +9,19 @@ data class LogEntry(
     val amount: Int,
     val unit: String
 )
+
+// parallel change
+sealed class LogEntry_ {
+    class Meal(
+        val id: Id,
+        val time: Long,
+        val description: String,
+        val amount: Int,
+        val unit: String
+    ) : LogEntry_()
+    class Nap(
+        val id: Id,
+        val time: Int,
+        val duration: Long
+    ) : LogEntry_()
+}
