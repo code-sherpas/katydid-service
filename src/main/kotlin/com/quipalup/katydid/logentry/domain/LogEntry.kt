05b642b45dfc9d59013c60795d7db98ba4c72980
Cristina Verdi <cristina.verdi.tobon@gmail.com>
2021-06-22 01:11:47 +0200

    Add child id to LogEntry_

--- src/main/kotlin/com/quipalup/katydid/logentry/domain/LogEntry.kt
+++ src/main/kotlin/com/quipalup/katydid/logentry/domain/LogEntry.kt
@@ -12,16 +12,23 @@ data class LogEntry(
 
 // parallel change
 sealed class LogEntry_ {
+    abstract val childId: ChildId
+
     class Meal(
         val id: Id,
+        override val childId: ChildId,
         val time: Long,
         val description: String,
         val amount: Int,
         val unit: String
     ) : LogEntry_()
+
     class Nap(
         val id: Id,
+        override val childId: ChildId,
         val time: Long,
         val duration: Long
     ) : LogEntry_()
 }
+
+data class ChildId(val value: Id)
