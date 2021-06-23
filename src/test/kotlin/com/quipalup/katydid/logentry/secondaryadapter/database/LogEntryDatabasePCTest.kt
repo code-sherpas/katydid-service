25c9e86a6e4fb6f501ede20f32c7f55bcd15734f
Cristina Verdi <cristina.verdi.tobon@gmail.com>
2021-06-24 00:50:51 +0200

    Apply lint

--- src/test/kotlin/com/quipalup/katydid/logentry/secondaryadapter/database/LogEntryDatabasePCTest.kt
+++ src/test/kotlin/com/quipalup/katydid/logentry/secondaryadapter/database/LogEntryDatabasePCTest.kt
@@ -1,60 +1,27 @@
 package com.quipalup.katydid.logentry.secondaryadapter.database
 
+import com.quipalup.katydid.logentry.domain.ChildId
 import com.quipalup.katydid.logentry.domain.LogEntry_
 import io.mockk.every
 import io.mockk.mockk
 import java.time.ZoneId
 import java.time.ZonedDateTime
-import java.util.*
+import java.util.UUID
 import org.assertj.core.api.Assertions.assertThat
-import org.assertj.core.api.recursive.comparison.RecursiveComparisonConfiguration
 import org.junit.jupiter.api.Test
 
 internal class LogEntryDatabasePCTest {
 
     @Test
     fun `search all log entries by child id`() {
-        //arrange : set up need data for performing the main action I'm testing
-        val mealId = UUID.randomUUID().toId()
-        val childId = UUID.randomUUID().toChildId()
-
-        val time = ZonedDateTime
-            .of(2021, 6, 23, 20, 30, 50, 4, ZoneId.of("UTC"))
-            .toEpochSecond()
-
-        val description = "Spaghetti bolognese"
-        val amount = 4
-        val unit = "grams"
-
-        val mealLogEntry = LogEntry_.Meal(
-            id = mealId,
-            childId = childId ,
-            time = time,
-            description = description,
-            amount = amount,
-            unit = unit
-        )
-
-        val napId = UUID.randomUUID().toId()
-
-        val napLogEntry = LogEntry_.Nap(
-            id = napId,
-            childId = childId,
-            time = time,
-            duration = 3
-        )
+        // arrange : set up need data for performing the main action I'm testing
         val expectedLogEntries = listOf(mealLogEntry, napLogEntry)
 
         val repository = mockk<JpaLogEntryRepositoryPC>()
         val logEntryDatabasePC = LogEntryDatabasePC(repository)
 
         // set up a stub: Test double that when invoked, returns a caned answer. Always returns the same answer
-        every {
-            repository.findAllByChildId(childId.value())
-        } returns listOf(
-            mealLogEntry.toJpa(),
-            napLogEntry.toJpa()
-        )
+        `will return jpaLogEntries for a named child`(repository, childId, mealLogEntry, napLogEntry)
 
         // act : Perform main action
         val logEntries = logEntryDatabasePC.searchAllByChildId(childId)
@@ -65,6 +32,50 @@ internal class LogEntryDatabasePCTest {
             .isEqualTo(expectedLogEntries)
     }
 
+    private fun `will return jpaLogEntries for a named child`(
+        repository: JpaLogEntryRepositoryPC,
+        childId: ChildId,
+        mealLogEntry: LogEntry_.Meal,
+        napLogEntry: LogEntry_.Nap
+    ) {
+        every {
+            repository.findAllByChildId(childId.value())
+        } returns listOf(
+            mealLogEntry.toJpa(),
+            napLogEntry.toJpa()
+        )
+    }
+
+    companion object {
+        private val childId = UUID.randomUUID().toChildId()
+        private val mealId = UUID.randomUUID().toId()
+
+        private val time = ZonedDateTime
+            .of(2021, 6, 23, 20, 30, 50, 4, ZoneId.of("UTC"))
+            .toEpochSecond()
+
+        private const val description = "Spaghetti bolognese"
+        private const val amount = 4
+        private const val unit = "grams"
+
+        private val mealLogEntry = LogEntry_.Meal(
+            id = mealId,
+            childId = childId,
+            time = time,
+            description = description,
+            amount = amount,
+            unit = unit
+        )
+
+        private val napId = UUID.randomUUID().toId()
+
+        private val napLogEntry = LogEntry_.Nap(
+            id = napId,
+            childId = childId,
+            time = time,
+            duration = 3
+        )
+    }
 }
 
 private fun LogEntry_.Nap.toJpa(): JpaLogEntryPC_ {
@@ -80,7 +91,7 @@ private fun LogEntry_.Meal.toJpa(): JpaLogEntryPC_ = JpaMealLogEntryPC(
     id = id.value,
     childId = childId.value(),
     time = time,
-    description = description ,
-    amount = amount ,
+    description = description,
+    amount = amount,
     unit = unit
 )
