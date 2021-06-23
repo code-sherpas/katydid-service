377f3af600378e7b871e648e2f93531c8075392b
Cristina Verdi <cristina.verdi.tobon@gmail.com>
2021-06-23 16:41:30 +0200

    Remove unused code

--- src/main/kotlin/com/quipalup/katydid/logentry/domain/LogEntryRepository.kt
+++ src/main/kotlin/com/quipalup/katydid/logentry/domain/LogEntryRepository.kt
@@ -2,7 +2,6 @@ package com.quipalup.katydid.logentry.domain
 
 import arrow.core.Either
 import com.quipalup.katydid.common.id.Id
-import com.quipalup.katydid.logentry.application.CreateLogEntriesError
 
 interface LogEntryRepository {
     fun findById(id: Id): Either<FindLogEntryError, LogEntry>
@@ -12,10 +11,4 @@ interface LogEntryRepository {
 }
 
 // parallel change
-interface LogEntryRepositoryPC {
-    fun findById(id: Id): Either<FindLogEntryError, LogEntry_>
-    fun deleteById(id: Id): Either<DeleteLogEntryError, Unit>
-    fun save(logEntry: LogEntry_): Either<SaveLogEntryError, Id>
-    fun existsById(id: Id): Boolean
-    fun saveAll(logEntries: List<LogEntry_>): Either<CreateLogEntriesError, List<Id>>
-}
+interface LogEntryRepositoryPC
