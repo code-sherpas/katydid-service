4ae3987b52d39429c8c1642ecf6e25593e3415cf
Cristina Verdi <cristina.verdi.tobon@gmail.com>
2021-06-28 21:40:51 +0200

    Search log entries, application layer (#30)
    * Green test: search log entries, application layer
    
    * fix wildcard import
    
    * lint

--- src/main/kotlin/com/quipalup/katydid/common/id/Id.kt
+++ src/main/kotlin/com/quipalup/katydid/common/id/Id.kt
@@ -1,5 +1,9 @@
 package com.quipalup.katydid.common.id
 
+import com.quipalup.katydid.logentry.domain.ChildId
 import java.util.UUID
 
 data class Id(val value: UUID = UUID.randomUUID())
+
+fun UUID.toId(): Id = Id(this)
+fun UUID.toChildId(): ChildId = ChildId(this.toId())
---
+++ src/main/kotlin/com/quipalup/katydid/logentry/application/SearchLogEntriesCommandHandler.kt
@@ -0,0 +1,15 @@
+package com.quipalup.katydid.logentry.application
+
+import com.quipalup.katydid.common.id.toChildId
+import com.quipalup.katydid.logentry.domain.LogEntryRepositoryPC
+import com.quipalup.katydid.logentry.domain.LogEntry_
+import java.util.UUID
+
+class SearchLogEntriesCommandHandler(val repository: LogEntryRepositoryPC) {
+    fun execute(searchLogEntriesCommand: SearchLogEntriesCommand): List<LogEntry_> =
+        searchLogEntriesCommand.childId.let {
+            UUID.fromString(it).toChildId()
+        }.let {
+            repository.searchAllByChildId(it)
+        }
+}
