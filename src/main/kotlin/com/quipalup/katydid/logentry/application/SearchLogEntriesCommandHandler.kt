947a0ed45cad56259cf2b14b0d30e51d762c1b8a
Cristina Verdi <cristina.verdi.tobon@gmail.com>
2021-06-28 12:11:34 +0200

    fix wildcard import

--- src/main/kotlin/com/quipalup/katydid/logentry/application/SearchLogEntriesCommandHandler.kt
+++ src/main/kotlin/com/quipalup/katydid/logentry/application/SearchLogEntriesCommandHandler.kt
@@ -3,7 +3,7 @@ package com.quipalup.katydid.logentry.application
 import com.quipalup.katydid.common.id.toChildId
 import com.quipalup.katydid.logentry.domain.LogEntryRepositoryPC
 import com.quipalup.katydid.logentry.domain.LogEntry_
-import java.util.*
+import java.util.UUID
 
 class SearchLogEntriesCommandHandler(val repository: LogEntryRepositoryPC) {
     fun execute(searchLogEntriesCommand: SearchLogEntriesCommand): List<LogEntry_> =
