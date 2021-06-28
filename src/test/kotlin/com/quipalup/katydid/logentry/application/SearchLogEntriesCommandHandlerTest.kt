d3fc678146e3f8c10c494e94f537fa062327ce8b
Cristina Verdi <cristina.verdi.tobon@gmail.com>
2021-06-29 00:33:56 +0200

    fix merge

--- src/test/kotlin/com/quipalup/katydid/logentry/application/SearchLogEntriesCommandHandlerTest.kt
+++ src/test/kotlin/com/quipalup/katydid/logentry/application/SearchLogEntriesCommandHandlerTest.kt
@@ -29,7 +29,7 @@ class SearchLogEntriesCommandHandlerTest {
     companion object {
         private val childId = UUID.randomUUID().toChildId()
         private val mealId = UUID.randomUUID().toId()
-        private val searchLogEntriesCommand = SearchLogEntriesCommand(childId.value().toString())
+        private val searchLogEntriesCommand = SearchLogEntriesByChildIdCommand(childId.value().toString())
         private val time = ZonedDateTime
             .of(2021, 6, 23, 20, 30, 50, 4, ZoneId.of("UTC"))
             .toEpochSecond()
