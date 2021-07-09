17f92d892c1c5b2d1a1e2875efd1495ebfe5e985
Cristina Verdi <cristina.verdi.tobon@gmail.com>
2021-07-09 16:28:37 +0200

    Insert nap log entries (#37)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,7 +4,7 @@ INSERT INTO log_entry
 VALUES(
     '93acd93f-cc03-49ab-b761-b2b2810ad505',
     '5ee62461-adb8-4618-a110-06290a787223',
-    1625787684
+    1625814857
 );
 
 INSERT INTO meal_log_entry
@@ -19,7 +19,7 @@ INSERT INTO log_entry
 VALUES(
     'd2ff5b08-572b-4559-bb1f-0b5cdf01480e',
     '86a93463-e7e1-4fc0-b12c-981f1eea16e8',
-    1625787684
+    1625829017
 );
 
 INSERT INTO meal_log_entry
@@ -35,7 +35,7 @@ INSERT INTO log_entry
 VALUES(
     '2da2e8c9-e698-4e02-8185-99972f858cf1',
     'b9c2380f-0b4c-4871-aefc-a6ed2c3a2408',
-    1625787684
+    1625828417
 );
 
 INSERT INTO meal_log_entry
@@ -50,7 +50,7 @@ INSERT INTO log_entry
 VALUES(
     '6f64212b-333a-4c27-a166-e2d9403c6bc9',
     'a5edf2fa-30b1-45e4-a39b-96243fa60caa',
-    1625787684
+    1625817617
 );
 
 INSERT INTO meal_log_entry
@@ -65,7 +65,7 @@ INSERT INTO log_entry
 VALUES(
     'a43dcdca-1840-4a11-b17c-d33ba0bdec4b',
     '666cf327-09da-46ad-a01c-d3ae6e8ebc9d',
-    1625787684
+    1625811677
 );
 
 INSERT INTO meal_log_entry
@@ -74,4 +74,74 @@ VALUES(
     'Watermelon and berries',
     'grams',
     'a43dcdca-1840-4a11-b17c-d33ba0bdec4b'
+);
+
+
+INSERT INTO log_entry
+VALUES(
+    '7a49cfd6-6a8a-404c-ad97-c51763341fe1',
+    '5ee62461-adb8-4618-a110-06290a787223',
+    1625819117
+);
+
+INSERT INTO nap_log_entry
+VALUES(
+    45,
+    '7a49cfd6-6a8a-404c-ad97-c51763341fe1'
+);
+
+INSERT INTO log_entry
+VALUES(
+    'd952c8d7-900f-46df-820c-a95910d8b2ee',
+    '86a93463-e7e1-4fc0-b12c-981f1eea16e8',
+    1625822837
+);
+
+
+INSERT INTO nap_log_entry
+VALUES(
+    60,
+    'd952c8d7-900f-46df-820c-a95910d8b2ee'
+);
+
+INSERT INTO log_entry
+VALUES(
+    '9ca4af5c-6fe5-4d86-b553-38e0917a22a2',
+    'b9c2380f-0b4c-4871-aefc-a6ed2c3a2408',
+    1625829017
+);
+
+
+INSERT INTO nap_log_entry
+VALUES(
+    20,
+    '9ca4af5c-6fe5-4d86-b553-38e0917a22a2'
+);
+
+INSERT INTO log_entry
+VALUES(
+    'fb6ead93-7a69-4341-837d-e020845f1983',
+    'a5edf2fa-30b1-45e4-a39b-96243fa60caa',
+    1625836517
+);
+
+
+INSERT INTO nap_log_entry
+VALUES(
+    120,
+    'fb6ead93-7a69-4341-837d-e020845f1983'
+);
+
+INSERT INTO log_entry
+VALUES(
+    '69d6da73-c8fb-4496-ae9b-ad70a4244df3',
+    '666cf327-09da-46ad-a01c-d3ae6e8ebc9d',
+    1625836517
+);
+
+
+INSERT INTO nap_log_entry
+VALUES(
+    35,
+    '69d6da73-c8fb-4496-ae9b-ad70a4244df3'
 );
