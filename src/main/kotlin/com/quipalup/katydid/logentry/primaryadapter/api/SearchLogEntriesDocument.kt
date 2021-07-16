f9e54626efd1f04dfe3793e81155296dbb70f536
ashikka <ashikagupta28@gmail.com>
2021-07-16 13:03:26 +0530

    feat: added included params

--- src/main/kotlin/com/quipalup/katydid/logentry/primaryadapter/api/SearchLogEntriesDocument.kt
+++ src/main/kotlin/com/quipalup/katydid/logentry/primaryadapter/api/SearchLogEntriesDocument.kt
@@ -1,21 +1,34 @@
 package com.quipalup.katydid.logentry.primaryadapter.api
 
-data class SearchLogEntriesDocument(val data: List<SearchLogEntryDocument> = listOf())
+data class SearchLogEntriesDocument(
+    val data: List<SearchLogEntryDocument> = listOf(),
+    val included: List<IncludedChildDocument> = listOf()
+)
 
 sealed class SearchLogEntryDocument {
     data class MealLogEntryDocument(
         val id: String,
         val type: String = "meal",
         val attributes: MealLogEntryDocumentAttributes,
-        val links: LogEntryLinksAttribute = LogEntryLinksAttribute("https://katydid-web-client.s3.us-east-2.amazonaws.com/icons/meal-icon.svg")
+        val links: LogEntryLinksAttribute = LogEntryLinksAttribute("https://katydid-web-client.s3.us-east-2.amazonaws.com/icons/meal-icon.svg"),
+        val relationships: LogEntryChildRelationship
     ) : SearchLogEntryDocument()
 
     data class NapLogEntryDocument(
         val id: String,
         val type: String = "nap",
         val attributes: NapLogEntryDocumentAttributes,
-        val links: LogEntryLinksAttribute = LogEntryLinksAttribute("https://katydid-web-client.s3.us-east-2.amazonaws.com/icons/nap-icon.svg")
+        val links: LogEntryLinksAttribute = LogEntryLinksAttribute("https://katydid-web-client.s3.us-east-2.amazonaws.com/icons/nap-icon.svg"),
+        val relationships: LogEntryChildRelationship
     ) : SearchLogEntryDocument()
+}
+
+sealed class IncludedChildDocument {
+    data class ChildIncluded(
+        val id: String,
+        val type: String = "child",
+        val attributes: ChildAttributes
+    ) : IncludedChildDocument()
 }
 
 data class MealLogEntryDocumentAttributes(
@@ -33,3 +46,18 @@ data class NapLogEntryDocumentAttributes(
 )
 
 data class LogEntryLinksAttribute(val iconURL: String)
+
+data class LogEntryChildRelationship(
+    val child: LogEntryChild
+)
+
+data class LogEntryChild(
+    val id: String,
+    val type: String = "child"
+)
+
+data class ChildAttributes(
+    val name: String,
+    val portraitURL: String,
+    val isPresent: Boolean
+)
--- src/main/kotlin/com/quipalup/katydid/logentry/primaryadapter/api/SearchLogEntriesEndpoint.kt
+++ src/main/kotlin/com/quipalup/katydid/logentry/primaryadapter/api/SearchLogEntriesEndpoint.kt
@@ -1,5 +1,10 @@
 package com.quipalup.katydid.logentry.primaryadapter.api
 
+import arrow.core.Either
+import com.quipalup.katydid.child.search.application.ChildResult
+import com.quipalup.katydid.child.search.application.FindChildByIdQuery
+import com.quipalup.katydid.child.search.application.FindChildByIdQueryHandler
+import com.quipalup.katydid.child.search.domain.FindChildByIdError
 import com.quipalup.katydid.logentry.application.SearchLogEntriesByChildIdCommand
 import com.quipalup.katydid.logentry.application.SearchLogEntriesCommandHandler
 import com.quipalup.katydid.logentry.domain.LogEntry_
@@ -9,30 +14,39 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class SearchLogEntriesEndpoint(
-    private val searchLogEntriesCommandHandler: SearchLogEntriesCommandHandler
+    private val searchLogEntriesCommandHandler: SearchLogEntriesCommandHandler,
+    private val findChildByIdQueryHandler: FindChildByIdQueryHandler
 ) {
 
     @GetMapping("log-entries")
     fun matchingCriteria(
         @RequestParam(required = false) parameters: Map<String, String>?
     ): SearchLogEntriesDocument {
-        print(parameters?.get("filter[childId]"))
-        return parameters!!.get("filter[childId]").toString()!!.run {
-            SearchLogEntriesByChildIdCommand(this).let {
-                searchLogEntriesCommandHandler.execute(it)
-            }.let {
-                val toSearchLogEntriesDocument = it.toSearchLogEntriesDocument()
-                toSearchLogEntriesDocument
+
+        val x = mapOf<String, String>(
+            "filter" to parameters!!.get("filter[childId]").toString()!!,
+            "included" to parameters!!.get("included").toString()
+        )
+        return x.run {
+            this.get("filter")!!.let {
+                SearchLogEntriesByChildIdCommand(it).let {
+                    val child = findChildByIdQueryHandler.execute(FindChildByIdQuery(it.childId))
+                    val logEntries = searchLogEntriesCommandHandler.execute(it)
+                    Pair(child, logEntries)
+                }.let {
+                    val toSearchLogEntriesDocument = it.toSearchLogEntriesDocument()
+                    toSearchLogEntriesDocument
+                }
             }
         }
     }
 }
 
-private fun List<LogEntry_>.toSearchLogEntriesDocument(): SearchLogEntriesDocument {
+fun Pair<Either<FindChildByIdError, ChildResult>, List<LogEntry_>>.toSearchLogEntriesDocument(): SearchLogEntriesDocument {
     return when {
-        this.isEmpty() -> SearchLogEntriesDocument()
+        this.second.isEmpty() -> SearchLogEntriesDocument()
         else -> SearchLogEntriesDocument(
-            data = this.map {
+            data = this.second.map {
                 when (it) {
                     is LogEntry_.Meal -> SearchLogEntryDocument.MealLogEntryDocument(
                         id = it.id.value.toString(),
@@ -42,6 +56,11 @@ private fun List<LogEntry_>.toSearchLogEntriesDocument(): SearchLogEntriesDocume
                             description = it.description,
                             amount = it.amount,
                             unit = it.unit
+                        ),
+                        relationships = LogEntryChildRelationship(
+                            LogEntryChild(
+                                id = it.childId.value().toString()
+                            )
                         )
                     )
                     is LogEntry_.Nap -> SearchLogEntryDocument.NapLogEntryDocument(
@@ -50,10 +69,31 @@ private fun List<LogEntry_>.toSearchLogEntriesDocument(): SearchLogEntriesDocume
                             childId = it.childId.value().toString(),
                             time = it.time,
                             duration = it.duration
+                        ),
+                        relationships = LogEntryChildRelationship(
+                            LogEntryChild(
+                                id = it.childId.value().toString()
+                            )
                         )
                     )
                 }
-            }
+            },
+            included = this.first.fold(
+                ifLeft = { listOf<IncludedChildDocument>() },
+                ifRight = {
+                    listOf(it.let {
+                        IncludedChildDocument.ChildIncluded(
+                            id = it.id,
+                            attributes = ChildAttributes(
+                                name = it.name,
+                                portraitURL = it.portraitURL,
+                                isPresent = it.isPresent
+                            )
+                        )
+                    }
+                    )
+                }
+            )
         )
     }
 }
