798a5abd0708ecdc80eb448855ffab0a84cc1841
ashikka <ashikagupta28@gmail.com>
2021-07-16 23:26:24 +0530

    feat: added stub for tests

--- src/test/kotlin/com/quipalup/katydid/child/FindChildByIdQueryHandlerTest.kt
+++ src/test/kotlin/com/quipalup/katydid/child/FindChildByIdQueryHandlerTest.kt
@@ -1,33 +1,71 @@
 package com.quipalup.katydid.child
 
+import arrow.core.Either
+import arrow.core.right
 import com.quipalup.katydid.child.ChildMother.BLANCA
 import com.quipalup.katydid.child.common.domain.Child
 import com.quipalup.katydid.child.common.domain.ChildRepository
+import com.quipalup.katydid.child.common.secondaryadapter.database.ChildDatabase
 import com.quipalup.katydid.child.search.application.ChildResult
 import com.quipalup.katydid.child.search.application.FindChildByIdQuery
 import com.quipalup.katydid.child.search.application.FindChildByIdQueryHandler
+import com.quipalup.katydid.child.search.domain.FindChildByIdError
+import com.quipalup.katydid.common.id.Id
+import io.mockk.every
 import io.mockk.mockk
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import java.net.URL
+import java.util.*
+
 
 internal class FindChildByIdQueryHandlerTest {
     @Test
     fun `find child by id`() {
 
-        print(FindChildByIdQueryHandler(childRepository))
+        val childRepository = mockk<ChildRepository>()
+        val childDatabase = ChildDatabase()
+
+
+        `will return child for the given id`(childRepository, Id(UUID.fromString(id)), child)
         FindChildByIdQueryHandler(childRepository).execute(findChildByIdQuery).fold(
             { Assertions.fail(it.toString()) },
             { assertThat(it).isEqualTo(expectedResult) }
         )
     }
 
+    private fun `will return child for the given id`(
+        repository: ChildRepository,
+        id: Id,
+        child: Child
+    ) {
+        every {
+            repository.findById(id)
+        } returns child.toStubResult()
+    }
+
     companion object {
+
         private val id = "5ee62461-adb8-4618-a110-06290a787223"
 
+        private val name = Child.Name("Blanca")
+        private val portraitURL =
+            Child.PortraitURL(URL("https://katydid-web-client.s3.us-east-2.amazonaws.com/img/profile/${id}.png"))
+
+        private val childId = Id(UUID.fromString(id))
+        private val isPresent = Child.IsPresent(true)
         private val findChildByIdQuery = FindChildByIdQuery(
             id = id
         )
+
+        private fun Child.toStubResult(): Either<FindChildByIdError, Child> =
+            Child(
+                id = id,
+                name = name,
+                portraitURL = portraitURL,
+                isPresent = isPresent
+            ).right()
 
         private fun Child.toResult(): ChildResult =
             ChildResult(
@@ -39,6 +77,12 @@ internal class FindChildByIdQueryHandlerTest {
 
         private val expectedResult: ChildResult = BLANCA.toResult()
 
-        private val childRepository: ChildRepository = mockk()
+        val child = Child(
+            id = childId,
+            name = name,
+            portraitURL = portraitURL,
+            isPresent = isPresent
+        )
+
     }
 }
